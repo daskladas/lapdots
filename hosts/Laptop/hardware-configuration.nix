bf1475013fbc29b9dab6f4cdaaf359e88f81f4a4
daskladas <xvzrsm@tutanota.de>
2025-11-14 21:10:11 +0100

    removed usb mount

--- hosts/Laptop/hardware-configuration.nix
+++ hosts/Laptop/hardware-configuration.nix
@@ -27,11 +27,6 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  fileSystems."/mnt/usb" =
-    { device = "/dev/disk/by-uuid/8A083BFB083BE4BB";
-      fsType = "ntfs";
-    };
-
   swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
